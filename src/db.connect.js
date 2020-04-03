7c7748d1fdebcdf0f1f4cdbcae448ca12991cf4b
Alexis Villegas Torres <alexisvt@gmail.com>
2020-04-03 04:04:46 -0600

    feat: add .env file support

--- src/db.connect.js
+++ src/db.connect.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 // Connect to MongoDB
 mongoose
-  .connect('mongodb://localhost:27017/mun', { useNewUrlParser: true })
+  .connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
